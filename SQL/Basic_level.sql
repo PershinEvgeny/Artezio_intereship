7b53130fc3272e5138d23fcd7103429cef31f314
Evgeny <casua1@mail.ru>
2019-06-09 20:11:56 +0300

    add new players

--- SQL/Basic_level.sql
+++ SQL/Basic_level.sql
@@ -6,3 +6,7 @@
  last_name varchar(30) NOT NULL,
  year_of_birth YEAR 
  );
+INSERT INTO players ( id, first_name, last_name) VALUES ( null, 'Artem', 'Dzyuba'); 
+INSERT INTO players ( id, first_name, last_name, year_of_birth) VALUES ( null, 'Igor', 'Akinfeev', 1986); 
+INSERT INTO players ( id, first_name, last_name, year_of_birth) VALUES ( null, 'Denis', 'Cheryshev', 1990); 
+INSERT INTO players ( id, first_name, last_name, year_of_birth) VALUES ( null, 'Igor', 'Smolnikov', 1988);
