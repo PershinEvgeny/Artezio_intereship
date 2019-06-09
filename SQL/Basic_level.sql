09b1bf408520c74bf80e3a52a691d77dd8dc96be
Evgeny <casua1@mail.ru>
2019-06-09 20:07:20 +0300

    add table "players"

--- SQL/Basic_level.sql
+++ SQL/Basic_level.sql
@@ -1,1 +1,8 @@
  CREATE DATABASE IF NOT EXISTS footballers;
+ USE footballers;
+ CREATE TABLE IF NOT EXISTS players (
+ id INT UNSIGNED NOT NULL auto_increment PRIMARY KEY,
+ first_name VARCHAR(30) NOT NULL,
+ last_name varchar(30) NOT NULL,
+ year_of_birth YEAR 
+ );
