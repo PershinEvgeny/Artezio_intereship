c1a1d348779e2c5313f887370361193311a9bc4d
Evgeny <casua1@mail.rugit config --global user.email casua1@mail.rugit config --global user.email casua1@mail.ru>
2019-07-10 11:34:34 +0300

    Задание 2. Взаимодействие с HTML. выведем сумму значений X1 и X2

--- JS/0 basic level/script.js
+++ JS/0 basic level/script.js
@@ -3,3 +3,17 @@ for(var i=10; i<=20; i++) {
 	sum+=i;
 }
 console.log(sum);
+function buttonClick() {
+	var x1 = parseInt(document.getElementById('x1').value);
+	var x2 = parseInt(document.getElementById('x2').value);
+	
+	if(Number.isNaN(x1) || Number.isNaN(x2)) {
+		alert("В поля х1 и х2 должны быть введены числовые значения.");
+	}
+	else {
+		var resultDiv = document.getElementById('result');
+		resultDiv.append("x1  + x2 = "+(x1+x2));
+	}
+	
+	
+}
