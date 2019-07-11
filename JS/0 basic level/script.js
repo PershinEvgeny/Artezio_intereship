15c7f42cd5174ee21dbf9d7776dd678adca2eb15
Evgeny <casua1@mail.ru>
2019-07-11 12:49:33 +0300

    Добавьте проверку на пустой ввод. Если пользователь не ввёл значения x1 или x2, выведите ошибку «Поля x1 и x2 должны быть заполнены».

--- JS/0 basic level/script.js
+++ JS/0 basic level/script.js
@@ -7,14 +7,16 @@ function buttonClick() {
 	clearDiv('result'); //очистка div результата
 	var x1 = parseInt(document.getElementById('x1').value);
 	var x2 = parseInt(document.getElementById('x2').value);
-	
-	if(Number.isNaN(x1) || Number.isNaN(x2)) {
+	if(document.getElementById('x1').value=="" || document.getElementById('x2').value=="") {
+		alert("Поля x1 и x2 должны быть заполнены");
+	}
+	else if(Number.isNaN(x1) || Number.isNaN(x2)) {
 		alert("В поля х1 и х2 должны быть введены числовые значения.");
 	}
 	else {
 		var resultDiv = document.getElementById('result');
 		resultDiv.append("x1  + x2 = "+(x1+x2));
-	}	
+	}
 }
 //функция очистки элемента div
 function clearDiv(elementID)
