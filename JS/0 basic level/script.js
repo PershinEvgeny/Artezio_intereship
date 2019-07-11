f0e3e3e85ed6db50b8a08139799003528c555f3b
Evgeny <casua1@mail.ru>
2019-07-11 11:56:15 +0300

    Доработайте скрипт таким образом, чтобы после каждого нажатия кнопки Run поле вывода результата очищалось от предыдущих значений

--- JS/0 basic level/script.js
+++ JS/0 basic level/script.js
@@ -4,6 +4,7 @@ for(var i=10; i<=20; i++) {
 }
 console.log(sum);
 function buttonClick() {
+	clearDiv('result'); //очистка div результата
 	var x1 = parseInt(document.getElementById('x1').value);
 	var x2 = parseInt(document.getElementById('x2').value);
 	
@@ -13,7 +14,10 @@ function buttonClick() {
 	else {
 		var resultDiv = document.getElementById('result');
 		resultDiv.append("x1  + x2 = "+(x1+x2));
-	}
-	
-	
+	}	
+}
+//функция очистки элемента div
+function clearDiv(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
 }
